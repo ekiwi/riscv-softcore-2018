b8a23ae730ca88f39112d5b78f16b033c2d14536
Kevin Läufer <laeufer@cs.berkeley.edu>
2019-03-05 15:25:08 -0800

    [engine-v] chisel: reimplement ALU
    Not tested / equivalence checked yet!

--- engine-v/chisel/src/main.scala
+++ engine-v/chisel/src/main.scala
@@ -3,5 +3,6 @@ object main {
     def main(args: Array[String]): Unit = {
         // generate verilog and save to file
         chisel3.Driver.execute(args, () => new addsub8)
+        chisel3.Driver.execute(args, () => new mf8_alu)
     }
 }
